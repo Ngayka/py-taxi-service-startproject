2cfda6bae06c315d9717872c7e4f0ccdf5e0a7fc
Ngayka <ngayka@gmail.com>
2025-04-25 08:16:18 +0300

    Solution and screenshot

--- taxi/models.py
+++ taxi/models.py
@@ -9,11 +9,19 @@ class Manufacturer(models.Model):
     name = models.CharField(max_length=63, unique=True)
     country = models.CharField(max_length=63)
 
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ['name']
 
 class Car(models.Model):
     manufacturer = models.ForeignKey(Manufacturer, on_delete=models.CASCADE)
     model = models.CharField(max_length=63)
     drivers = ManyToManyField(settings.AUTH_USER_MODEL)
+
+    def __str__(self):
+        return f"{self.model}: {self.manufacturer.name}"
 
     class Meta:
         ordering = ["model"]
@@ -21,3 +29,6 @@ class Car(models.Model):
 
 class Driver(AbstractUser):
     license_number = models.CharField(max_length=63, unique=True)
+
+    def __str__(self):
+        return self.username
